8d71627efc90c514a50b9698464e12d223e77941
Mona Lisa <holmesmainsend@gmail.com>
2023-02-26 12:14:09 -0500

    creating separate queue class

--- BFS.java
+++ BFS.java
@@ -1,2 +1,33 @@
-public class BFS {   
+import java.util.LinkedList;
+
+public class BFS {
+
+    public static LinkedList<Integer>[] adj_list(int n, int[][] edges) {
+        LinkedList<Integer>[] graph = new LinkedList[n];
+        for(int i = 0; i < n; i++) {
+            graph[i] = new LinkedList<>();
+        }
+
+        for(int[] e : edges) {
+            int a = e[0];
+            int b = e[1];
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+
+        return graph;
+    }
+    
+    public static void main(String[] args) {
+        int n = 5;
+        int[][] edges = {{0, 1}, {0, 2}, {0, 4}, {1, 2}, {1, 3}, {2, 3}};
+        LinkedList<Integer>[] graph = adj_list(n, edges);
+        
+        boolean[] visited = new boolean[n];
+        for(int i = 0; i < n; i++) {
+         visited[i] = false;
+        }
+ 
+        bfs(graph, visited);
+    } 
 }
