47f108910db377284edd0cf04638cd0c2c5f2a0d
Mona Lisa <holmesmainsend@gmail.com>
2023-02-26 12:31:23 -0500

    finished custom Queue class

--- Queue.java
+++ Queue.java
@@ -1,3 +1,25 @@
+import java.util.LinkedList;
+
 public class Queue {
-       
+    LinkedList<Integer> queue = new LinkedList<>();
+
+    public void enqueue(int o) {
+        this.queue.add(o);
+    }
+    
+    public void dequeue() {
+        this.queue.remove();
+    }
+
+    public int front() {
+        return this.queue.element();
+    }
+
+    public boolean isEmpty() {
+        return this.queue.size() == 0;
+    }
+
+    public int size() {
+        return this.queue.size();
+    }
 }
